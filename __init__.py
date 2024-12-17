35a9184322ff7596766afac0e723e413f05eae3f
Jeff <alpyne.dreams@gmail.com>
2024-12-16 22:00:04 -0800

    Bump version to v1.0.1

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ bl_info = {
     "author" : "Alpyne",
     "description" : "Source Engine glTF tools.",
     "blender" : (4, 2, 0),
-    "version" : (1, 0, 0),
+    "version" : (1, 0, 1),
     "location" : "",
     "warning" : "",
     "category" : "Generic"
